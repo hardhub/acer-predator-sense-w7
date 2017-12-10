0d8ddda46bf2450402574848e482033ca27350d4
Maxim <maxim@bitoxygen.com>
2017-12-11 01:07:47 +0300

    Upload source code.

---
+++ Projects/Setup/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Setup
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Running running = new Running();
+            running.Go(args);
+        }
+    }
+}
---
+++ Projects/Setup/Running.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+using BOOTSTRAPTOR;
+
+
+namespace Setup
+{
+    internal class Running
+    {
+        public void Go(string[] args)
+        {
+            Assembly assembly = this.GetAssembly();
+
+            // Setup
+            Type setup = assembly.GetType("BOOTSTRAPTOR.Setup");
+
+            // Version
+            FieldInfo field = setup.GetField("gOSversion", BindingFlags.Static | BindingFlags.NonPublic);
+            Console.WriteLine($"Current version: {field.GetValue(null)}");
+
+            field.SetValue(null, "6.1");
+            Console.WriteLine($"Updated version: {field.GetValue(null)}");
+
+            // Main
+            MethodInfo main = setup.GetMethod("Main");
+            main.Invoke(null, new object[] { args });
+        }
+
+        private Assembly GetAssembly()
+        {
+            return typeof(Messages).Assembly;
+        }
+    }
+}
